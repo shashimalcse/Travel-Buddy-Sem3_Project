20a466b7bdfd1f1d8dec11c91200a12b01b945c7
shashimal <shashimalsenarath.17@cse.mrt.ac.lk>
2019-05-29 14:44:07 +0530

    singleton

---
+++ App/Controllers/GuideSearch.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Comtrollers;
+
+use \App\Models\User;
+use \Core\View;
+
+class GuideSearch extends \Core\Controller{
+
+    public static function showAction(){
+
+
+    }
+}
--- Core/Model.php
+++ Core/Model.php
@@ -5,31 +5,44 @@ namespace Core;
 use PDO;
 use App\Config;
 
-/**
- * Base model
- *
- * PHP version 7.0
- */
-abstract class Model
+
+ class Model
 {
 
-    /**
-     * Get the PDO database connection
-     *
-     * @return mixed
-     */
-    protected static function getDB()
-    {
-        static $db = null;
 
-        if ($db === null) {
+    // protected static function getDB()
+    // {
+    //     static $db = null;
+
+    //     if ($db === null) {
+    //         $dsn = 'mysql:host=' . Config::DB_HOST . ';dbname=' . Config::DB_NAME . ';charset=utf8';
+    //         $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
+
+    //         // Throw an Exception when an error occurs
+    //         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //     }
+
+    //     return $db;
+    // }
+    protected static $db;
+
+
+    private function __construct() {
+        try {
             $dsn = 'mysql:host=' . Config::DB_HOST . ';dbname=' . Config::DB_NAME . ';charset=utf8';
-            $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
+            self::$db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
+            self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "MySql Connection Error: " . $e->getMessage();
+        }
+    }
 
-            // Throw an Exception when an error occurs
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    public static function getDB() {
+        if (!self::$db) {
+            new Model();
         }
 
-        return $db;
+        return self::$db;
     }
+
 }
